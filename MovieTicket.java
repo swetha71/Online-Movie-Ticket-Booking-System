d68059ac0f10816967d3c7c5d7d579746dd0fdf2
swetha71 <60966801+swetha71@users.noreply.github.com>
2020-05-15 22:40:46 +0530

    Update MovieTicket.java

--- MovieTicket.java
+++ MovieTicket.java
@@ -2,7 +2,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-
 public class MovieTicket extends Frame 
 {
 	Button MovieTicket;
@@ -12,7 +11,7 @@ public class MovieTicket extends Frame
 	Statement statement;
 	public MovieTicket() 
 	{
-		try 
+	       try 
 		{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} 
@@ -21,27 +20,25 @@ public class MovieTicket extends Frame
 			System.err.println("Cannot find and load driver");
 			System.exit(1);
 		}
-		connectToDB();
-	}
-
-	public void connectToDB() 
-    {
-		try 
-		{
-		  connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","swetha","manager");
-		  statement = connection.createStatement();
-
-		} 
-		catch (SQLException connectException) 
-		{
-		  System.out.println(connectException.getMessage());
-		  System.out.println(connectException.getSQLState());
-		  System.out.println(connectException.getErrorCode());
-		  System.exit(1);
-		}
-    }
-	public void buildGUI() 
-	{		
+	      connectToDB();
+	      }
+             public void connectToDB() 
+               {
+		  try 
+		   {
+		     connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","swetha","manager");
+		    statement = connection.createStatement();
+                   } 
+		   catch (SQLException connectException) 
+                    {
+		    System.out.println(connectException.getMessage());
+		    System.out.println(connectException.getSQLState());
+		    System.out.println(connectException.getErrorCode());
+		    System.exit(1);
+		  }
+           }
+	   public void buildGUI() 
+	   {   		
 		MovieTicket = new Button("Buy Ticket");
 		MovieTicket.addActionListener(new ActionListener() 
 		{
@@ -57,17 +54,14 @@ public class MovieTicket extends Frame
 				{
 				  displaySQLErrors(insertException);
 				}
-			}
+			}  
 		});
-
-		buidTf = new TextField(15);
+                buidTf = new TextField(15);
 		tickidTf = new TextField(15);
 		whenTf = new TextField(15);
 		rateTf = new TextField(15);		
-		
 		errorText = new TextArea(10, 40);
 		errorText.setEditable(false);
-
 		Panel first = new Panel();
 		first.setLayout(new GridLayout(4, 2));
 		first.add(new Label("Buyer ID:"));
@@ -78,46 +72,35 @@ public class MovieTicket extends Frame
 		first.add(whenTf);
 		first.add(new Label("rate:"));
 		first.add(rateTf);
-		
 		first.setBounds(125,90,200,100);
-		
 		Panel second = new Panel(new GridLayout(4, 1));
 		second.add(MovieTicket);
-        second.setBounds(125,220,150,100);         
-				
+        second.setBounds(125,220,150,100);         		
 		setLayout(null);
-
 		add(first);
 		add(second);
-	    
 		setTitle("Buying tickets");
 		Color clr = new Color(100, 100, 140);
 		setBackground(clr); 
 		setSize(500, 600);
 		setVisible(true);
-		
 	}
-	
 	private void displaySQLErrors(SQLException e) 
 	{
-		errorText.append("\nSQLException: " + e.getMessage() + "\n");
+	        errorText.append("\nSQLException: " + e.getMessage() + "\n");
 		errorText.append("SQLState:     " + e.getSQLState() + "\n");
 		errorText.append("VendorError:  " + e.getErrorCode() + "\n");
 	}
-
-	
-
 	public static void main(String[] args) 
 	{
 		MovieTicket bb = new MovieTicket();
-
 		bb.addWindowListener(new WindowAdapter(){
 		  public void windowClosing(WindowEvent e) 
 		  {
 			System.exit(0);
 		  }
 		});
-		
 		bb.buildGUI();
 	}
+
 }
